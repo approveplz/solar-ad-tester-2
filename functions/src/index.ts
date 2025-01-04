8f56e53eec35d94eb4940effee74579c52ccf112
afz <al.zhang93@gmail.com>
2025-01-04 13:02:40 -0300

    move unused solar create ad on click request to bottom for code organization

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -44,195 +44,6 @@ initializeApp({
     storageBucket: 'solar-ad-tester-2.appspot.com',
 });
 
-export const createAdFromClickRequest = onRequest(
-    {
-        cors: true,
-        timeoutSeconds: 60,
-        memory: '512MiB',
-    },
-    async (req, res) => {
-        const {
-            adArchiveId,
-            videoHdUrl,
-            videoSdUrl,
-            videoPreviewImageUrl,
-            adTitle,
-            adBody,
-            ctaType,
-            pageName,
-            pageId,
-            pageLikeCount,
-            hasUserReported,
-            startDateUnixSeconds,
-            endDateUnixSeconds,
-            publisherPlatform,
-        } = req.body;
-
-        const scrapedAd: ParsedFbAdInfo = {
-            adArchiveId,
-            videoHdUrl,
-            videoSdUrl,
-            videoPreviewImageUrl,
-            adTitle,
-            adBody,
-            ctaType,
-            pageName,
-            pageId,
-            pageLikeCount,
-            hasUserReported,
-            startDateUnixSeconds,
-            endDateUnixSeconds,
-            publisherPlatform,
-        };
-
-        const metaAdCreatorServiceSolar = new MetaAdCreatorService({
-            appId: process.env.FACEBOOK_APP_ID || '',
-            appSecret: process.env.FACEBOOK_APP_SECRET || '',
-            accessToken: process.env.FACEBOOK_ACCESS_TOKEN || '',
-            accountId: process.env.FACEBOOK_ACCOUNT_ID || '',
-            apiVersion: '20.0',
-        });
-
-        try {
-            /* Generate hash of video */
-            const scrapedVideoFileUrl =
-                scrapedAd.videoHdUrl || scrapedAd.videoSdUrl;
-
-            const videoHash = await generateVideoHash(scrapedVideoFileUrl, 1);
-            const uploadedVideoHashHap = await getVideoHashMapFirestore(
-                'SOLAR'
-            );
-
-            /* Check if we have already tested this video */
-            if (videoHash in uploadedVideoHashHap) {
-                const existingAdName = uploadedVideoHashHap[videoHash];
-                console.log(
-                    `This video has already been uploaded. Video hash: ${videoHash}. Existing ad name: ${existingAdName}`
-                );
-                res.status(500).send({ code: 'DUPLICATE' });
-            }
-
-            const campaignParams = {
-                objective: 'OUTCOME_LEADS',
-                status: 'PAUSED',
-            };
-
-            const promotedObjectParams = {
-                pixelId: '700671091822152',
-                customEventType: 'LEAD',
-                pageId: '117903068075583',
-            };
-
-            const adSetParams = {
-                bidAmountCents: 2200,
-                optimizationGoal: 'OFFSITE_CONVERSIONS',
-                billingEvent: 'IMPRESSIONS',
-                dailyBudgetCents: '2000',
-                lifetimeBudgetCents: '2000',
-                bidStrategy: 'COST_CAP',
-            };
-
-            const ctaLinkValue =
-                'https://www.greenenergycollective.org/s/no-cost-solar-v3';
-            const videoMessage = `Homeowners, would you trade 30 seconds for $8,500?
-
-The recently revamped "2024 Solar Incentive Program" means you can now have solar panels installed at no cost on your roof, and make a major cut in your energy bills
-
-All you have to do is click the link below to find out if you qualify (takes 30 seconds or less)
-
-${ctaLinkValue}`;
-
-            const adCreativeParams = {
-                videoTitle:
-                    'Homeowners In Your Area Are Getting Paid to Go Solar',
-                videoMessage,
-                linkDescription: 'Less than 30 seconds to Qualify',
-                ctaType: 'LEARN_MORE',
-                ctaLinkValue,
-            };
-
-            const fbAdSettings: FbAdSettings = {
-                campaignParams,
-                promotedObjectParams,
-                adSetParams,
-                adCreativeParams,
-            };
-
-            // Create Campaign
-
-            // const campaignName = `[facebook] - [Solar] - [AZ] - [MOM] - [ALL] - [SOLAR-SHS-V3] - APP - 1`;
-            // const campaign: Campaign =
-            //     await metaAdCreatorService.createCampaign({
-            //         name: campaignName,
-            //         fbAdSettings,
-            //     });
-            // const campaignId = campaign.id; // Campaign ID if we create it here
-
-            const campaignId = '120210470839980108'; // Real Campaign ID
-            // const campaignId = '120210773404130108'; // Test Campaign ID
-
-            const adSetNameAndAdName = `AZ-S-${scrapedAd.adArchiveId}`;
-
-            const adSet: AdSet | undefined =
-                await metaAdCreatorServiceSolar.createAdSet({
-                    name: adSetNameAndAdName,
-                    campaignId,
-                    fbAdSettings,
-                });
-
-            invariant(adSet, 'adSet must be defined');
-
-            // Create Ad Video
-            const adVideo: AdVideo =
-                await metaAdCreatorServiceSolar.uploadAdVideo({
-                    scrapedAdArchiveId: scrapedAd.adArchiveId,
-                    videoFileUrl: scrapedVideoFileUrl,
-                });
-
-            const adCreative: AdCreative =
-                await metaAdCreatorServiceSolar.createAdCreative(
-                    `Creative-${adSetNameAndAdName}`,
-                    adVideo,
-                    scrapedAd.videoPreviewImageUrl,
-                    fbAdSettings
-                );
-
-            const ad: Ad = await metaAdCreatorServiceSolar.createAd({
-                name: adSetNameAndAdName,
-                adSet,
-                adCreative,
-            });
-
-            const { fileCloudStorageUri } = await uploadVideoToStorage(
-                `${adSetNameAndAdName}.mp4`,
-                scrapedVideoFileUrl
-            );
-
-            const createdFbAd: CreatedFbAdInfo = {
-                campaignId,
-                adSetId: adSet.id,
-                adSetName: adSetNameAndAdName,
-                creativeId: adCreative.id,
-                adId: ad.id,
-                videoId: adVideo.id,
-                videoCloudStorageUri: fileCloudStorageUri,
-                videoHash,
-            };
-
-            await saveFbAdFirestore('SOLAR', scrapedAd, createdFbAd);
-            await saveVideoHashFirestore(
-                'SOLAR',
-                videoHash,
-                adSetNameAndAdName
-            );
-
-            res.status(200).send({ code: 'CREATED' });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send(error);
-        }
-    }
-);
 
 const handleCreateAd = async (
     adType: string,
@@ -595,3 +406,196 @@ export const createImageAdFromHttp = onRequest(async (req, res) => {
         });
     }
 });
+
+
+/* No longer used. This was for solar */
+export const createAdFromClickRequest = onRequest(
+    {
+        cors: true,
+        timeoutSeconds: 60,
+        memory: '512MiB',
+    },
+    async (req, res) => {
+        const {
+            adArchiveId,
+            videoHdUrl,
+            videoSdUrl,
+            videoPreviewImageUrl,
+            adTitle,
+            adBody,
+            ctaType,
+            pageName,
+            pageId,
+            pageLikeCount,
+            hasUserReported,
+            startDateUnixSeconds,
+            endDateUnixSeconds,
+            publisherPlatform,
+        } = req.body;
+
+        const scrapedAd: ParsedFbAdInfo = {
+            adArchiveId,
+            videoHdUrl,
+            videoSdUrl,
+            videoPreviewImageUrl,
+            adTitle,
+            adBody,
+            ctaType,
+            pageName,
+            pageId,
+            pageLikeCount,
+            hasUserReported,
+            startDateUnixSeconds,
+            endDateUnixSeconds,
+            publisherPlatform,
+        };
+
+        const metaAdCreatorServiceSolar = new MetaAdCreatorService({
+            appId: process.env.FACEBOOK_APP_ID || '',
+            appSecret: process.env.FACEBOOK_APP_SECRET || '',
+            accessToken: process.env.FACEBOOK_ACCESS_TOKEN || '',
+            accountId: process.env.FACEBOOK_ACCOUNT_ID || '',
+            apiVersion: '20.0',
+        });
+
+        try {
+            /* Generate hash of video */
+            const scrapedVideoFileUrl =
+                scrapedAd.videoHdUrl || scrapedAd.videoSdUrl;
+
+            const videoHash = await generateVideoHash(scrapedVideoFileUrl, 1);
+            const uploadedVideoHashHap = await getVideoHashMapFirestore(
+                'SOLAR'
+            );
+
+            /* Check if we have already tested this video */
+            if (videoHash in uploadedVideoHashHap) {
+                const existingAdName = uploadedVideoHashHap[videoHash];
+                console.log(
+                    `This video has already been uploaded. Video hash: ${videoHash}. Existing ad name: ${existingAdName}`
+                );
+                res.status(500).send({ code: 'DUPLICATE' });
+            }
+
+            const campaignParams = {
+                objective: 'OUTCOME_LEADS',
+                status: 'PAUSED',
+            };
+
+            const promotedObjectParams = {
+                pixelId: '700671091822152',
+                customEventType: 'LEAD',
+                pageId: '117903068075583',
+            };
+
+            const adSetParams = {
+                bidAmountCents: 2200,
+                optimizationGoal: 'OFFSITE_CONVERSIONS',
+                billingEvent: 'IMPRESSIONS',
+                dailyBudgetCents: '2000',
+                lifetimeBudgetCents: '2000',
+                bidStrategy: 'COST_CAP',
+            };
+
+            const ctaLinkValue =
+                'https://www.greenenergycollective.org/s/no-cost-solar-v3';
+            const videoMessage = `Homeowners, would you trade 30 seconds for $8,500?
+
+The recently revamped "2024 Solar Incentive Program" means you can now have solar panels installed at no cost on your roof, and make a major cut in your energy bills
+
+All you have to do is click the link below to find out if you qualify (takes 30 seconds or less)
+
+${ctaLinkValue}`;
+
+            const adCreativeParams = {
+                videoTitle:
+                    'Homeowners In Your Area Are Getting Paid to Go Solar',
+                videoMessage,
+                linkDescription: 'Less than 30 seconds to Qualify',
+                ctaType: 'LEARN_MORE',
+                ctaLinkValue,
+            };
+
+            const fbAdSettings: FbAdSettings = {
+                campaignParams,
+                promotedObjectParams,
+                adSetParams,
+                adCreativeParams,
+            };
+
+            // Create Campaign
+
+            // const campaignName = `[facebook] - [Solar] - [AZ] - [MOM] - [ALL] - [SOLAR-SHS-V3] - APP - 1`;
+            // const campaign: Campaign =
+            //     await metaAdCreatorService.createCampaign({
+            //         name: campaignName,
+            //         fbAdSettings,
+            //     });
+            // const campaignId = campaign.id; // Campaign ID if we create it here
+
+            const campaignId = '120210470839980108'; // Real Campaign ID
+            // const campaignId = '120210773404130108'; // Test Campaign ID
+
+            const adSetNameAndAdName = `AZ-S-${scrapedAd.adArchiveId}`;
+
+            const adSet: AdSet | undefined =
+                await metaAdCreatorServiceSolar.createAdSet({
+                    name: adSetNameAndAdName,
+                    campaignId,
+                    fbAdSettings,
+                });
+
+            invariant(adSet, 'adSet must be defined');
+
+            // Create Ad Video
+            const adVideo: AdVideo =
+                await metaAdCreatorServiceSolar.uploadAdVideo({
+                    scrapedAdArchiveId: scrapedAd.adArchiveId,
+                    videoFileUrl: scrapedVideoFileUrl,
+                });
+
+            const adCreative: AdCreative =
+                await metaAdCreatorServiceSolar.createAdCreative(
+                    `Creative-${adSetNameAndAdName}`,
+                    adVideo,
+                    scrapedAd.videoPreviewImageUrl,
+                    fbAdSettings
+                );
+
+            const ad: Ad = await metaAdCreatorServiceSolar.createAd({
+                name: adSetNameAndAdName,
+                adSet,
+                adCreative,
+            });
+
+            const { fileCloudStorageUri } = await uploadVideoToStorage(
+                `${adSetNameAndAdName}.mp4`,
+                scrapedVideoFileUrl
+            );
+
+            const createdFbAd: CreatedFbAdInfo = {
+                campaignId,
+                adSetId: adSet.id,
+                adSetName: adSetNameAndAdName,
+                creativeId: adCreative.id,
+                adId: ad.id,
+                videoId: adVideo.id,
+                videoCloudStorageUri: fileCloudStorageUri,
+                videoHash,
+            };
+
+            await saveFbAdFirestore('SOLAR', scrapedAd, createdFbAd);
+            await saveVideoHashFirestore(
+                'SOLAR',
+                videoHash,
+                adSetNameAndAdName
+            );
+
+            res.status(200).send({ code: 'CREATED' });
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
+    }
+);
+
