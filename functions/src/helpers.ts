32eede7ed08fa35ce1585c4933c46c6c21d42c31
afz <al.zhang93@gmail.com>
2025-02-18 15:06:16 -0300

    remove unused helpers

--- functions/src/helpers.ts
+++ functions/src/helpers.ts
@@ -1,71 +1,8 @@
 import ffmpeg from 'fluent-ffmpeg';
 import ffmpegPathImport from 'ffmpeg-static';
-import crypto from 'crypto';
-import fetch from 'node-fetch';
-import { Readable, PassThrough } from 'stream';
-
 const ffmpegPath = ffmpegPathImport as unknown as string;
 
 ffmpeg.setFfmpegPath(ffmpegPath);
-
-export async function generateVideoHash(
-    videoUrl: string,
-    sampleIntervalSeconds: number = 1
-) {
-    const response = await fetch(videoUrl);
-    if (!response.ok) {
-        throw new Error(
-            `HTTP error: ${response.status} ${response.statusText}`
-        );
-    }
-    if (!response.body) {
-        throw new Error('Network error: Response body is null');
-    }
-
-    const readableStream = Readable.from(response.body);
-
-    return new Promise<string>((resolve, reject) => {
-        const combinedHash = crypto.createHash('sha256');
-        let frameCount = 0;
-        const outputStream = new PassThrough();
-
-        ffmpeg(readableStream)
-            .videoFilter(`fps=1/${sampleIntervalSeconds}`) // Outputs one frame every sampleIntervalSeconds
-            .outputOptions(['-f', 'rawvideo', '-pix_fmt', 'rgb24']) // Outputs to raw video and sets pixel format to rgb24, which is common
-            .on('error', (error) => {
-                console.error('ffmpeg processing error', error);
-                reject(error);
-            })
-            .on('end', () => {
-                console.log(`ffmpeg processing finished`);
-                const finalHash = combinedHash.digest('hex');
-                resolve(finalHash);
-            })
-            .pipe(outputStream);
-
-        outputStream
-            .on('data', (chunk) => {
-                const frameHash = crypto
-                    .createHash('md5')
-                    .update(chunk)
-                    .digest('hex');
-
-                console.log({ frameHash });
-                combinedHash.update(frameHash);
-                frameCount++;
-                console.log(`Processed frame ${frameCount}`);
-            })
-            .on('error', (error) => {
-                console.error(`Output stream error`, error);
-                reject(error);
-            });
-
-        readableStream.on('error', (error) => {
-            console.error(`Imput stream error`, error);
-            reject(error);
-        });
-    });
-}
 
 export function getAdName(
     counter: number,
