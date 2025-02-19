4c0b07f9be5338a56a3c8877870b2b2751a2de4f
afz <al.zhang93@gmail.com>
2025-02-19 13:23:19 -0300

    rename filename for scraped ad in firestore and update schema

--- functions/src/models/ScrapedAdDataFirestore.ts
+++ functions/src/models/ScrapedAdDataFirestore.ts
@@ -1,16 +1,17 @@
 export interface ScrapedAdDataFirestore {
     url: string;
+    // videoIdentifer is also the Doc ID
     videoIdentifier: string;
     adArchiveId: string;
     startTimeUnixSeconds: number;
     formattedStartTime: string;
     description: string;
-    isUsed: boolean;
-    wantToUse?: boolean;
+    isUsedForAd: boolean;
+    processed: boolean;
     pageName: string;
     pageId: string;
-    // categorization: string;
     textTranscript: string;
     hook: string;
     duplicateVideoIdentifiers?: string[];
+    descriptionEmbedding?: number[];
 }
