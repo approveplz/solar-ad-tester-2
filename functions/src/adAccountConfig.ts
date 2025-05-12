7306163fda2227eb8a55a6945e798ed85156af88
afz <al.zhang93@gmail.com>
2025-05-12 10:43:42 -0600

    add new ad accounts

--- functions/src/adAccountConfig.ts
+++ functions/src/adAccountConfig.ts
@@ -52,6 +52,26 @@ export const AD_ACCOUNT_DATA: AdAccountConfigData = {
             },
         },
     },
+    '358423827304360': {
+        name: 'Vincent x Digitsolution CC 2 New', // Marcus, Roofing
+        type: 'R',
+        campaignId: '120217711802790271',
+        scalingCampaignId: '',
+        targeting: {
+            excluded_custom_audiences: [
+                {
+                    id: '120226871220770271',
+                    name: 'Roofing Lead (180d)',
+                },
+            ],
+            brand_safety_content_filter_levels: ['FEED_RELAXED'],
+            targeting_relaxation_types: {
+                lookalike: 0,
+                custom_audience: 0,
+            },
+        },
+    },
+
     '8653880687969127': {
         name: 'Vincent x Digitsolution CC 2',
         type: 'R',
@@ -62,6 +82,22 @@ export const AD_ACCOUNT_DATA: AdAccountConfigData = {
                 location_types: ['home', 'recent'],
                 // TODO: Change this to the correct location cluster ID
                 location_cluster_ids: [{ key: '28950427651210969' }],
+            },
+            brand_safety_content_filter_levels: ['FEED_RELAXED'],
+            targeting_relaxation_types: {
+                lookalike: 0,
+                custom_audience: 0,
+            },
+        },
+    },
+    '661505182923254': {
+        name: 'AWL_RN_FB_ABG-342749',
+        type: 'O',
+        campaignId: '120215328779990104',
+        scalingCampaignId: '',
+        targeting: {
+            geo_locations: {
+                location_types: ['home', 'recent'],
             },
             brand_safety_content_filter_levels: ['FEED_RELAXED'],
             targeting_relaxation_types: {
