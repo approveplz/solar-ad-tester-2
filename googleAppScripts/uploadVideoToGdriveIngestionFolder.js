8ab6170661d5e46e8416ef673d70ac3e2a1ef721
afz <al.zhang93@gmail.com>
2025-06-14 13:16:38 -0700

    unarchive script

--- googleAppScripts/uploadVideoToGdriveIngestionFolder.js
+++ googleAppScripts/uploadVideoToGdriveIngestionFolder.js
@@ -4,11 +4,11 @@ const INGESTION_FOLDER_ID = '1AwBk7bOjyuBVlfTVxZ-t4wE2IatX8O22';
 function doGet(e) {
     try {
         const fileUrl = e.parameter.fileUrl;
-        const videoIdentifier = e.parameter.videoIdentifier;
-        const fileName = `R-AZ-AZ-AZ-${videoIdentifier}.mp4`;
+        const baseAdName = e.parameter.baseAdName;
+        const hookName = e.parameter.hookName;
+        const fileName = `${hookName}-${baseAdName}.mp4`;
         const response = UrlFetchApp.fetch(fileUrl);
         const blob = response.getBlob().setName(fileName);
-
         const folder = DriveApp.getFolderById(INGESTION_FOLDER_ID);
         const file = folder.createFile(blob);
 
